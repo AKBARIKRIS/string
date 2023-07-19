cdb0367a166d364940f1ea2fc9c97243134bc124
AKBARIKRIS <137590850+AKBARIKRIS@users.noreply.github.com>
2023-07-19 04:33:43 -0700

    Add files via upload

---
+++ string1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	char ch[]="KRIS";
+	int i;
+	for(i=0;i<=3;i++) 
+	{
+		printf("%c",ch[i]+32);
+	 } 
+	
+	
+}
---
+++ string2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	char ch[]="kris";
+	int i;
+	for(i=0;i<=3;i++) 
+	{
+		printf("%c",ch[i]-32);
+	 } 
+	
+	
+}
